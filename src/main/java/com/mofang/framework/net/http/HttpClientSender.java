4e564cd506314b4d8b2fee395d16c9909d101871
zhaodexin <milo@127.0.0.1>
2015-01-06 15:52:00 +0800

    update

--- src/main/java/com/mofang/framework/net/http/HttpClientSender.java
+++ src/main/java/com/mofang/framework/net/http/HttpClientSender.java
@@ -94,6 +94,43 @@ public class HttpClientSender
 		}
 	}
 	
+	public static String post(CloseableHttpClient client, String url, String postData, String contentType) throws Exception
+	{
+		CloseableHttpResponse response = null;
+		HttpPost action = null;
+		try
+		{
+			action = buildHttpPost(url, postData, contentType);
+			if(null == action)
+				return null;
+
+			response = client.execute(action, new BasicHttpContext());
+			HttpEntity entity = response.getEntity();
+            if (null == entity)
+            	return null;
+            
+            byte[] bytes = EntityUtils.toByteArray(entity);
+            return new String(bytes);
+		}
+		catch(Exception e)
+		{
+			action.abort();
+			return null;
+		}
+		finally
+		{
+			if(null != response)
+			{
+				try
+				{
+					response.close();
+				}
+				catch (IOException e) 
+				{}
+			}
+		}
+	}
+	
 	private static HttpGet buildHttpGet(String url)
 	{
 		try
@@ -120,6 +157,26 @@ public class HttpClientSender
 			ProtocolVersion protocolVersion = new ProtocolVersion(version.getProtocol(), version.getMajor(), version.getMinor());
 			post.setProtocolVersion(protocolVersion);
 			post.addHeader(HTTP.CONN_KEEP_ALIVE, "true");
+			HttpEntity entity = new StringEntity(postData, "UTF-8");
+			post.setEntity(entity);
+			return post;
+		}
+		catch(Exception e)
+		{
+			return null;
+		}
+	}
+	
+	private static HttpPost buildHttpPost(String url, String postData, String contentType)
+	{
+		try
+		{
+			HttpPost post = new HttpPost(url);
+			HttpVersion version = HttpVersion.HTTP_1_1;
+			ProtocolVersion protocolVersion = new ProtocolVersion(version.getProtocol(), version.getMajor(), version.getMinor());
+			post.setProtocolVersion(protocolVersion);
+			post.addHeader(HTTP.CONN_KEEP_ALIVE, "true");
+			post.addHeader(HTTP.CONTENT_TYPE, contentType);
 			HttpEntity entity = new StringEntity(postData, "UTF-8");
 			post.setEntity(entity);
 			return post;
